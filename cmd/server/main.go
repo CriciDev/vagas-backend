d0817cf9006de876e24eeeedf63ff651fefdf5bc
juanc <juancarlosbatistazeferino@gmail.com>
2026-05-03 22:56:35 -0300

    feat: add basic main composition root

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
 
+	"github.com/CriciumaDevJobs/backend/infra"
+	"github.com/CriciumaDevJobs/backend/internal/devs"
+)
 
-func main(){
-	
+func main() {
+
+	db := infra.InitDB()
+
+	devController := devs.InitializeDevController(db)
+
+	fmt.Println("Iniciando servidor")
+
+	http.HandleFunc("/devs", func(w http.ResponseWriter, r *http.Request) {
+		devController.CreateDev(context.Background(), w, r)
+	})
+
+	fmt.Println("Servidor iniciado")
+	http.ListenAndServe(":8080", nil)
 }
