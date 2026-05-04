179f59d50a09885b7d379ae01b97203436cd9e2a
juanc <juancarlosbatistazeferino@gmail.com>
2026-05-03 22:52:10 -0300

    feat: add raw dev repository

--- internal/devs/repository.go
+++ internal/devs/repository.go
@@ -1,1 +1,25 @@
 package devs
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/lib/pq"
+)
+
+type DevRepository struct {
+	DB *sql.DB
+}
+
+func NewDevRepository(db *sql.DB) *DevRepository {
+	repository := DevRepository{
+		DB: db,
+	}
+
+	return &repository
+}
+
+func (repository *DevRepository) CreateDev(ctx context.Context, dev *Dev) error {
+	query := "INSERT INTO devs (name, email, skills, bio, availability, socials) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;"
+	return repository.DB.QueryRowContext(ctx, query, dev.Name, dev.Email, dev.Skills, dev.Bio, dev.Availability, pq.Array(dev.Socials)).Scan(&dev.ID)
+}
