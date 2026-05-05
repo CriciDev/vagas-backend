3b95818fd96791cbf6ee4e75629bf2ea5d430f75
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-05 14:36:11 -0300

    feat: add authentication request and response structs

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,13 @@
 package auth
+
+import "time"
+
+type AuthenticationRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthenticationResponse struct {
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
