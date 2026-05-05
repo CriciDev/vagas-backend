a52d3cd40f5487cb045d657fb108addf6c1a18e3
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-05 14:38:33 -0300

    feat: change dev errors to follow new http error structure

--- handlers/dev_errors.go
+++ handlers/dev_errors.go
@@ -1,11 +1,15 @@
 package handlers
 
-import "errors"
+import (
+	"net/http"
+)
 
 var (
-	ErrNameNotEmpty      = errors.New("Campo Nome deve ser preenchido!")
-	ErrEmailNotEmpty     = errors.New("Campo Email deve ser preenchido!")
-	ErrEmailAlreadyInUse = errors.New("Email já está em uso")
-	ErrBioNotEmpty       = errors.New("Campo Biografia deve ser preenchido")
-	ErrPasswordToLong    = errors.New("O Campo de Senha não pode ultrapassar 72 caracteres")
+	ErrNameNotEmpty           = NewError(http.StatusUnprocessableEntity, "Campo Nome deve ser preenchido!")
+	ErrEmailNotEmpty          = NewError(http.StatusUnprocessableEntity, "Campo Email deve ser preenchido!")
+	ErrEmailAlreadyInUse      = NewError(http.StatusUnprocessableEntity, "Email já está em uso")
+	ErrBioNotEmpty            = NewError(http.StatusUnprocessableEntity, "Campo Biografia deve ser preenchido")
+	ErrPasswordToLong         = NewError(http.StatusUnprocessableEntity, "O Campo de Senha não pode ultrapassar 72 caracteres")
+	ErrInvalidEmailOrPassword = NewError(http.StatusUnauthorized, "Email ou Senha inválidos")
+	ErrProfileNotFound        = NewError(http.StatusNotFound, "Houve algum erro ao encontrar seu Perfil!")
 )
