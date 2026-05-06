adce2629a16449bd4bd39cc6b8a4e1a5c0c4f809
juan.zeferino <juan.zeferino@angeloni.com.br>
2026-05-06 16:44:27 -0300

    fix: remove if statement to check if the string has more than 72 characters and now returns an error from core library

--- utils/password_encoder.go
+++ utils/password_encoder.go
@@ -1,26 +1,19 @@
 package utils
 
 import (
+	"errors"
 	"log"
-	"net/http"
-	"unicode/utf8"
 
-	"github.com/CriciumaDevJobs/backend/handlers"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func EncryptPassword(password string) (string, *handlers.ErrorResponse) {
-
-	//bcrypt encoder trunkate strings with more than 72 characters
-	if utf8.RuneCountInString(password) > 72 {
-		return "", handlers.ErrPasswordToLong
-	}
+func EncryptPassword(password string) (string, error) {
 
 	hashedpassword, bcryptErr := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if bcryptErr != nil {
 		log.Printf("ERRO: Falha ao encriptar senha! Message: %s", bcryptErr.Error())
-		return "", handlers.NewError(http.StatusInternalServerError, "Erro interno!")
+		return "", errors.New("Erro Interno!")
 	}
 
 	return string(hashedpassword), nil
